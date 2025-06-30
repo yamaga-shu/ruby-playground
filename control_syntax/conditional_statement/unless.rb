e34a426b05c75396e19d792e29dcb13b850ea8e8
shu.yamaga <s.yamaga.0318@gmail.com>
2025-07-01 08:32:06 +0900

    bc Add:  example demonstrating Ruby `unless` statement usage

---
+++ control_syntax/conditional_statement/unless.rb
@@ -0,0 +1,22 @@
+x = 11
+
+# `unless` statement
+unless x == 10
+  puts 'Variable x is NOT 10.'
+end
+## Output:
+## Variable x is NOT 10.
+
+# `unless` Expression
+message = unless x == 10
+  'Variable x is NOT 10.'
+end
+
+puts message
+## Output:
+## Variable x is NOT 10.
+
+# Postfix `unless` statement
+puts 'Variable x is NOT 10.' unless x == 10
+## Output:
+## Variable x is NOT 1 0.
